7668b78e638dd4d79c3c8ee63b4498c10a5c495d
Thomas Gale <thomasgale2005@gmail.com>
2022-03-24 23:19:46 +0000

    commit on master

--- index.js
+++ index.js
@@ -1,16 +1,38 @@
+
+function convertVHToPX(vh) {
+	return vh*document.documentElement.clientHeight/100;
+}
 function pickUp(item){
-    if(item=='pick'){
-        
+    if(item=='shortHook'){
+        itemHeld = item
+        document.getElementById("shortHook").style.position = "absolute";
+    } else {
+        console.log("not shortHook");
+    }
+}
+function pickUp(item){
+    if(item=='shortHook'){
+        itemHeld = item
+        document.getElementById("shortHook").style.position = "absolute";
+    } else {
+        console.log("not shortHook");
     }
 }
 var itemHeld = null;
-var pointerX = -1;
-var pointerY = -1;
+var cursorPosX = -1;
+var cursorPosY = -1;
+console.log(itemHeld);
 document.onmousemove = function(event) {
-    pointerX = event.pageX;
-    pointerY = event.pageY;
+    cursorPosX = event.pageX;
+    cursorPosY = event.pageY;
 }
-setInterval(pointerCheck, 1000);
+setInterval(pointerCheck, 50);
 function pointerCheck() {
-    console.log('Cursor at: '+pointerX+', '+pointerY);
+    if (itemHeld != null) {
+        document.getElementById(itemHeld).style.top = (cursorPosY - convertVHToPX(3.5)) + "px";
+        document.getElementById(itemHeld).style.left = (cursorPosX - convertVHToPX(56.5)) + "px";
+    } else {
+        // console.log(itemHeld);
+    } 
+    console.log('Cursor at: '+cursorPosX+', '+cursorPosY);
 }
